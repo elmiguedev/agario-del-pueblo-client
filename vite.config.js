d1b758a24602558a2966cc9e2918db49656858b0
florenciacarrillo <florencianoelcarrillo@gmail.com>
2023-10-23 22:33:10 -0300

    feat: deploy

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './',
+  build: {
+    minify: "terser",
+    terserOptions: {
+      format: {
+        comments: false,
+      },
+    },
+  },
 });
